e353b1ce2d6895a10145416a1472159e1b89de33
Vasily93 <markovavasily@gmail.com>
2020-12-13 17:57:18 -0800

    signup and login routes

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require("connect-flash");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Joi = require('joi');
 const Campgroud = require('./models/campground');
 const Review = require('./models/review');
+const User = require('./models/user');
 const {campgroundSchema, reviewSchema} = require('./schemas');
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+const users = require('./routes/users');
 
 
 //mongo db connection
@@ -54,22 +59,33 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use((req, res, next) => {
    res.locals.success = req.flash('success');
    res.locals.error = req.flash('error');
    next()
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
-//Routes below
-
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({email: 'vasily@gmail.com' , username: 'vasya'})
+    const newUser = await User.register(user, 'shopito')
+    res.send(newUser);
 })
 
+//Routes below
+
+app.use('/campgrounds', campgrounds);
+app.use('/', users);
+app.use('/campgrounds/:id/reviews', reviews);
+
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
+    next(new ExpressError('Page Not FoundED', 404))
 })
 
 //Error handler
